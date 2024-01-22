161582c4fe07310c053899abd9e212e03c9491dd
S m, Aruna <arun.s.m.cse@gmail.com>
2024-01-22 17:54:18 -0600

    Compile and package
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>

--- src/main/java/com/arsulegai/filereader/FileReaderApplication.java
+++ src/main/java/com/arsulegai/filereader/FileReaderApplication.java
@@ -2,8 +2,9 @@ package com.arsulegai.filereader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FileReaderApplication {
   public static void main(String[] args) {
     SpringApplication.run(FileReaderApplication.class, args);
