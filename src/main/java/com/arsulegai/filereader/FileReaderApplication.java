2ab169215cb1ad012126b6af5b1c4825f2e46125
S m, Aruna <arun.s.m.cse@gmail.com>
2024-01-23 19:19:08 -0600

    Add scheduled logic
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>

--- src/main/java/com/arsulegai/filereader/FileReaderApplication.java
+++ src/main/java/com/arsulegai/filereader/FileReaderApplication.java
@@ -3,8 +3,10 @@ package com.arsulegai.filereader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableScheduling
 public class FileReaderApplication {
   public static void main(String[] args) {
     SpringApplication.run(FileReaderApplication.class, args);
--- src/main/java/com/arsulegai/filereader/service/reader/impl/ReaderServiceImpl.java
+++ src/main/java/com/arsulegai/filereader/service/reader/impl/ReaderServiceImpl.java
@@ -7,6 +7,7 @@ import com.arsulegai.filereader.service.storage.StorageService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,7 @@ public class ReaderServiceImpl implements ReaderService {
 
   @Override
   @Transactional
+  @Scheduled(fixedRate = 5000)
   public boolean readFiles() {
     // TODO: process these files asynchronously, do not block the API
     // list the files from the storage service
